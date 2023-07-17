a161eda52db29f21a295ce6c0d20d8fac2537a44
vanytsvetkov <ivanfromearth@gmail.com>
2023-07-17 11:26:14 +0300

    Added a mechanism for processing long responses from the BIRD socket.

--- exasrc/exabird.go
+++ exasrc/exabird.go
@@ -6,7 +6,31 @@ import (
 	log "github.com/sirupsen/logrus"
 	"regexp"
 	"strings"
+	"time"
 )
+
+func birdQueryWithTimeout(socketPath string, query string, timeout time.Duration) ([]byte, error) {
+	resultChan := make(chan []byte, 1)
+	errChan := make(chan error, 1)
+
+	go func() {
+		reply, err := birdsocket.Query(socketPath, query)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		resultChan <- reply
+	}()
+
+	select {
+	case reply := <-resultChan:
+		return reply, nil
+	case err := <-errChan:
+		return nil, err
+	case <-time.After(timeout):
+		return nil, fmt.Errorf("birdsocket query timed out")
+	}
+}
 
 func birdHandler(prefix *string) []string {
 	socket := "/var/run/bird/bird.ctl"
@@ -14,7 +38,8 @@ func birdHandler(prefix *string) []string {
 	birdQuery := fmt.Sprintf("show route for %v primary all", *prefix)
 	log.Debugf("Making BIRD query: %s", birdQuery)
 
-	replyBinary, err := birdsocket.Query(socket, birdQuery)
+	timeout := 15 * time.Second
+	replyBinary, err := birdQueryWithTimeout(socket, birdQuery, timeout)
 	if err != nil {
 		log.Error(err)
 		return []string{defaultASN}
@@ -22,20 +47,16 @@ func birdHandler(prefix *string) []string {
 	reply := string(replyBinary)
 	log.Debugf("%s", reply)
 
-	if reply == "Network not found" {
-		return []string{defaultASN}
-	} else {
-		// common case: ^\s+BGP.(\w+):\s+(.+)\s*$
-		re := regexp.MustCompile(`BGP\.as_path:\s([\s\d+]*)`)
+	// common case: ^\s+BGP.(\w+):\s+(.+)\s*$
+	re := regexp.MustCompile(`BGP\.as_path:\s([\s\d+]*)`)
 
-		match := re.FindStringSubmatch(reply)
-		if len(match) > 1 {
-			group := strings.TrimSpace(match[1])
-			//log.Debugf("as_path: %s", group)
-			return strings.Split(group, " ")
-		} else {
-			return []string{defaultASN}
-		}
+	match := re.FindStringSubmatch(reply)
+	if len(match) > 1 {
+		group := strings.TrimSpace(match[1])
+		//log.Debugf("as_path: %s", group)
+		return strings.Split(group, " ")
+	} else {
+		return []string{defaultASN}
 	}
 
 }
